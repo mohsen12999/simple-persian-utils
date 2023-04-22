3a4d8d8a89eee8c103e327f44f08a1f54e4a82ae
GiriMadhan <girimadhan7@gmail.com>
2023-04-22 23:53:47 +0530

    feat: added function to convert english to persian digits.

--- src/digit-utils.ts
+++ src/digit-utils.ts
@@ -10,3 +10,16 @@ export const convertPersianDigit2English = (inputString: string):string =>
         .replace(/۷/g,"7")
         .replace(/۸/g,"8")
         .replace(/۹/g,"9");
+
+export const convertEnglishDigit2Persian = (inputString: string) =>
+    inputString
+        .replace(/0/g, "۰")
+        .replace(/1/g, "۱")
+        .replace(/2/g, "۲")
+        .replace(/3/g, "۳")
+        .replace(/4/g, "۴")
+        .replace(/5/g, "۵")
+        .replace(/6/g, "۶")
+        .replace(/7/g, "۷")
+        .replace(/8/g, "۸")
+        .replace(/9/g, "۹");
